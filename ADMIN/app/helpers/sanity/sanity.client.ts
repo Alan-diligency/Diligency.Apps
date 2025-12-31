6f90e29ed18008b0d9d1600e677002a2f85aa185
Alan-diligency <ardenoy.alan@gmail.com>
2025-12-31 18:23:19 +0100

    Fixed Sanity token usage on Admin

--- ADMIN/app/helpers/sanity/sanity.client.ts
+++ ADMIN/app/helpers/sanity/sanity.client.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client';
 export const SANITY_CLIENT = createClient({
-    projectId: '623q79br',
+    projectId: 'bap4muo0',
     dataset: 'production',
-    token: 'skHx3ZQzwJMq8mY3f75uWEOwmjj5bLun17ZrADBY0VKN0YzSVmaW6M1T884xaRDLzz9amAs6wKA47yFyW0Fwx3fOxZ3UntSpNWnhtSwRgutiCizBQsaQ0qD1zgjMcTe6sMkXhBKxTR527SqfIevnzF52txlRWxIK2zsTw21sQ8YQuqdpFgDp',
+    token: 'kEG6PgxbibTihtnEJxkdkzNaBkoUyw17ORKqMRFNxsd81xhdETXU69GFeFCPC5xLHKXvCFVqz4HpFYjhJS9vhBf0uvvVKu1meIF0AmPM9XYmUD9IEP4inYpapHBR8erQoZ4R59BIiC5KHBq4kZHDI6Mo2w6nOT9VgzG1TN5RafjDUOPZzAN',
     apiVersion: '2025-02-19',
     useCdn: false
 });
